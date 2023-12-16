f19e744a65afa3e2aeb53e89705ec292e1cbcd76
Marwanali100 <152684710+Marwanali100@users.noreply.github.com>
2023-12-16 11:39:18 +0300

    Update lec2.py
    مروان الواقدي

--- lec2.py
+++ lec2.py
@@ -36,15 +36,7 @@ class App(QWidget):
         self.height=480
         self.initUI()
         
-     def initUI(self):
-         self.setWindowTitle(self.title)
-         self.setGeometry(self.left, self.top,  self.width, self.height )
-         self.show()
-         
-if __name__ == '__main__':
-     app=QApplication(sys.argv)   
-     ex=App()
-     sys.exit(app.exec_())
+    
 
 -------------------------------------------------------
 import PyQt5
